93b770d72c9a37ba6818613816f50ed8ffaa6ca2
Mahi Islam <112865015+mmmahiii@users.noreply.github.com>
2022-12-06 21:24:50 +0000

    remove fixed

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os,sys
 
 array = ["","","","","","","","","",""] #array of set size 10
 x = 0 #back pointer
-y = 0 #front pointer
 
 def view(): #prints the entire array
 	print(array)
 
 def add():
-	global x,y
+	global x
 	while (x <= 9): # proceeds to add only if the pointer isnt at the end of the array, so no values are added if the list is full
 		addValue = input("Enter the value to add to the list: ")
 		array[x] = addValue # changes the pointer empty space to the inputed value
@@ -18,11 +17,11 @@ def add():
 		print("The list is full") # if the pointer is at the end of the array then the list is full and nothing gets added
 
 def remove():
-	global x,y
-	while (x != y): # only proceeds to remove if the front pointer isnt at the same position as the back pointer
+	global x
+	while (x != 0): # only proceeds to remove if the pointer isnt at the beginning of the array, so if the array isnt empty
 		print("Your value has been removed")
-		array[y] = "" # the value on the front pointer is removed/ replaced with ""
-		y += 1 # the front pointer moves up one
+		array[x-1] = "" # the value on the front pointer is removed/ replaced with ""
+		x -=1 # the front pointer moves up one
 		break
 	else:
 		print("There is nothing in the list to remove") # if the front and back pointer are at the same positions then the list is empty and nothing gets removed
